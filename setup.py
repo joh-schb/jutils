41158bce5ed941e74c4e42981a7e8b86faf3f30e
Johannes Fischer <joh.fischer@outlook.com>
2023-08-29 16:05:35 +0200

    change form dev to version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="jutils",
-    version="dev",
+    version="0.9",
     packages=find_packages(),
     url="https://github.com/joh-fischer/jutils",
     license="MIT License",
