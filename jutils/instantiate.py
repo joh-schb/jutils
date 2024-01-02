edd69c1ad175f2bdf3e57e44cd30b195d79a7081
Johannes Fischer <joh.fischer@outlook.com>
2024-01-02 15:41:07 +0100

    make separate file for instantiation

---
+++ jutils/instantiate.py
@@ -0,0 +1,21 @@
+import importlib
+
+
+def get_obj_from_str(string, reload=False):
+    module, cls = string.rsplit(".", 1)
+    if reload:
+        module_imp = importlib.import_module(module)
+        importlib.reload(module_imp)
+    return getattr(importlib.import_module(module, package=None), cls)
+
+
+def instantiate_from_config(config):
+    if "target" not in config:
+        raise KeyError("Expected key `target` to instantiate.")
+    return get_obj_from_str(config["target"])(**config.get("params", dict()))
+
+
+if __name__ == "__main__":
+    # instantiate_from_config(config)
+    cfg = dict(target="jutils.helpers.Namespace", params=dict(a=1, b=2))
+    print("instantiate_from_config(cfg):", instantiate_from_config(cfg))
