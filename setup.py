9c9a991ee3837dfe500b8f9693e24cdffa17be66
Johannes Fischer <joh.fischer@outlook.com>
2025-09-29 18:25:27 +0200

    move to new version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="jutils",
-    version="0.9.3.1",
+    version="0.9.4.0",
     packages=find_packages(),
     url="https://github.com/joh-schb/jutils",
     license="MIT License",
